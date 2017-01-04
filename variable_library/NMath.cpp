0bad918c2be7e855f23e59c6752e3f8feb37c6de
NomotoMao <kyanata0707@gmail.com>
2017-01-04 12:00:32 +0900

    【NMathクラスの更新】
    ・Sin関数の追加
    ・Cos関数の追加
    ・Tan関数の追加
    ・Atan2関数の追加
    ・Log関数の追加
    ・各関数の変数名などの変更

--- variable_library/NMath.cpp
+++ variable_library/NMath.cpp
@@ -1,20 +1,23 @@
 #include "NMath.h"
+#include <stdio.h>
+
 
 using namespace variableNS;
 
-double NMath::pow_result = 1;
-int NMath::cnt = 1;
+double NMath::powResult = 1;
+int NMath::mCount = 1;
+int NMath::factResult = 1;
 
 double NMath::Pi() {
 	return 3.14159265359;
 }
 
 double NMath::Floor(double x) {
-	return (int)(x < 0.0 ? x - 0.9 : x);
+	return (int)(x < 0.0 ? x - 0.9 : x);//切り下げて返す
 }
 
 double NMath::Round(double x) {
-	return (int)(x < 0.0 ? x : x + 0.9);
+	return (int)(x < 0.0 ? x : x + 0.9);//切り上げて返す
 }
 
 float NMath::Max(float x, float y) {
@@ -29,17 +32,17 @@ float NMath::Max(float x, float y) {
 //まだできてない
 float NMath::Max(float x[],int y) {
 	
-	if (x[0] < x[cnt]) {
-		x[0] = x[cnt];
+	if (x[0] < x[mCount]) {
+		x[0] = x[mCount];
 	}
-	cnt++;
+	mCount++;
 	y--;
 	if (y == 0) {
-		cnt = 1;
+		mCount = 1;//初期化
 		return x[0];
 	}
 
-	return NMath::Max(x, y);
+	return Max(x, y);
 }
 
 float NMath::Min(float x, float y) {
@@ -54,17 +57,17 @@ float NMath::Min(float x, float y) {
 //まだできてない
 float NMath::Min(float x[],int y) {
 	
-	if (x[0] > x[cnt-1]) {
-		x[0] = x[cnt-1];
+	if (x[0] > x[mCount-1]) {
+		x[0] = x[mCount-1];
 	}
-	cnt++;
+	mCount++;
 	y--;
 	if (y == 0) {
-		cnt = 1;
+		mCount = 1;
 		return x[0];
 	}
 
-	return NMath::Min(x, y);
+	return Min(x, y);
 }
 
 double NMath::Ads(double x) {
@@ -81,11 +84,27 @@ double NMath::Ads(double x) {
 
 }
 
+double NMath::Fact(int x) {
+
+	factResult = factResult *x;
+
+	x--;
+
+	if (x == 0) {
+		int i = factResult;//factResultを初期化する前に代入をする
+		factResult = 1;//初期化
+		return i;
+	}
+
+	return Fact(x);
+
+}
+
 double NMath::Sqrt(double x) {
 
 	double s1 = 1, s2;
 	if (x <= 0) {
-		return 0;/*エラー処理*/
+		return 0;//Error
 	}
 	do
 	{
@@ -109,9 +128,10 @@ double NMath::Clamp(double x, double min, double max) {
 }
 
 double NMath::Repeat(double x, double min, double max) {
+
 	if (x > max) {
 		x -= (max - min);
-		return NMath::Repeat(x, min, max);
+		return Repeat(x, min, max);
 	}
 
 	return x;
@@ -127,59 +147,141 @@ double NMath::Lerp(float a, float b, float t) {
 
 }
 
-//まだできてない
+
+
 double NMath::Log(double x) {
-	return 0;
+
+	int i, t;
+	double r = 0.0;
+
+	for (i = 0; i < 13; i++) {
+		t = i * 2 + 1;
+		r += Pow( (x - 1.0)/(x + 1.0), t) / double(t);
+	}
+
+	return 2.0 * r;
 }
 
 double NMath::Pow(double x, int y) {
 
-
-	if (y == 0) return 1;
+	if (y <= 0) return 0;//Error
+	if (y == 0) return powResult;
 		
-	NMath::pow_result *= x;
+	powResult *= x;
 	y--;
-	if (y != 0) return NMath::Pow(x, y);
+	if (y != 0) return Pow(x, y);
 
-	double i = NMath::pow_result;
-	NMath::pow_result = 1;
+	double i = powResult;//powResultを初期化する前に代入をする
+	powResult = 1;//初期化
 		return i;
 }
 
 double NMath::DegToRad(double x) {
-	return x*(NMath::Pi() / 180);
+	return x*(Pi() / 180);
 }
 
 double NMath::RadToDeg(double x) {
-	return x*(180 / NMath::Pi());
+	return x*(180 / Pi());
 }
 
-double NMath::Sin(double x) {
-	return 0;
+double NMath::Sin(double rad) {
+
+	double t, y;
+	int n;
+
+	y = rad;
+	t = rad;
+	n = 2;
+
+	while (1)
+	{
+		t = -t*rad*rad / ((2 * n - 2)*(2 * n - 1));
+		if (Ads(t) <= 0.00001) break;
+		y = y + t;
+		n++;
+	}
+	return y;
+}
+
+double NMath::Cos(double rad) {
+	double t, y;
+	int n;
+
+	y = 1.0;
+	t = 1.0;
+	n = 1;
+
+	while (1) {
+		t = -t*rad*rad / ((2 * n)*(2 * n - 1));
+		if (Ads(t) <= 0.00001) break;
+		y = y + t;
+		n++;
+	}
+	return y;
 
 }
 
-double NMath::Cos(double x) {
-	return 0;
+double NMath::Tan(double rad) {
+	
+	return Sin(rad) / Cos(rad);
+}
+//できていない
+double NMath::ASin(double rad) {
+	if (rad <= -1 || rad >= 1) return 0;
+	
+	return 1;
+
+}
+//できていない
+double NMath::ACos(double rad) {
+	if (rad <= -1 || rad >= 1) return 0;
+
+	return 1;
 
 }
 
-double NMath::Tan2(double x) {
-	return 0;
+double NMath::ATan2(double a , double b) {
+	if (a < 0) {
+		// 引数がマイナスのケースをなくしておく
+		if (b < 0) {
+			// 両方マイナスなら、符号を入れ替えてそのまま計算
+			a = -a;
+			b = -b;
+		}
+		else {
+			return -ATan2(-a, b);
+		}
+	}
+	else if (b < 0) {
+		return -ATan2(a, -b);
+	}
+	else if (a > b) {
+		// 45度以上の場合は収束しないので逆数を使う
+		return 90 - ATan2(b, a);
+	}
 
-}
+	double u = (double)a / b;
 
-double NMath::ASin(double x) {
-	return 0;
+	if (a * 3 >= b * 2) {
+		// 45度に近いと収束が悪いので、2倍角の公式から導出される代替式を使う
+		double v = (Sqrt(1.0 + u * u) - 1.0) / u;
+		return 2.0 * ATan2((int)(v * 2000 + 0.5), 2000);
 
-}
-
-double NMath::ACos(double x) {
-	return 0;
-
-}
-
-double NMath::ATan2(double x) {
-	return 0;
-
+		return v;
+	}
+	/*
+	double p = 57.295778965948;  // 180 / π
+	double ret = 0.0;
+	double v = u;
+	double d = 99.0;
+	int c = 1;
+	while (d >= 0.1 || d <= -0.1) {
+		d = p * v / c;
+		ret += d;
+		v *= u * u;
+		c = (c < 0 ? 2 : -2) - c;
+	}
+	//return ret;
+	return NMath::DegToRad(ret);
+	*/
 }
