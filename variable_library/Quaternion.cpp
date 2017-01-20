6f756acf167f5b94b0b7dd8c96ce9eeb80136f9d
unknown <nwuser@GM2150059.da.iwasaki.ac.jp>
2017-01-20 09:29:22 +0900

    【Quaternionクラスの一応の完成】
    ・27の発表会で使うであろう機能は完成しました

--- variable_library/Quaternion.cpp
+++ variable_library/Quaternion.cpp
@@ -13,6 +13,8 @@ Quaternion::Quaternion(int xx, int yy, int zz, int ww) {
 	y = yy;
 	z = zz;
 	w = ww;
+
+	const_cast<Vector3&>(this->euler) = this->ToVector3();
 }
 
 Quaternion::Quaternion(float xx, float yy, float zz, float ww) {
@@ -20,6 +22,8 @@ Quaternion::Quaternion(float xx, float yy, float zz, float ww) {
 	y = yy;
 	z = zz;
 	w = ww;
+
+	const_cast<Vector3&>(this->euler) = this->ToVector3();
 }
 
 Quaternion::Quaternion(Vector4 vec) {
@@ -27,6 +31,8 @@ Quaternion::Quaternion(Vector4 vec) {
 	y = vec.y;
 	z = vec.z;
 	w = vec.w;
+
+	const_cast<Vector3&>(this->euler) = this->ToVector3();
 }
 
 Quaternion::~Quaternion() {
@@ -38,10 +44,12 @@ Quaternion::~Quaternion() {
 void Quaternion::Identity() {
 	this->x = this->y = this->z = 0;
 	this->w = 1;
+
+	const_cast<Vector3&>(this->euler) = this->ToVector3();
 }
 
-//絶対値
-float Quaternion::Norm() {
+//長さ
+float Quaternion::Length() {
 	return sqrt(this->w * this->w + this->x * this->x + this->y * this->y + this->z * this->z);
 }
 
@@ -51,17 +59,79 @@ void Quaternion::Set(Quaternion quaternion) {
 	this->y = quaternion.y;
 	this->z = quaternion.z;
 	this->w = quaternion.w;
-}
 
-//オイラー角で回転
-void Quaternion::EulerAngles(Vector3 vec) {
-	//this->x = 
+	const_cast<Vector3&>(this->euler) = this->ToVector3();
 }
 
 //各オペレータ
 void Quaternion::operator =(Quaternion q) {
 	this->Set(q);
+	const_cast<Vector3&>(this->euler) = this->ToVector3();
 }
+
+//任意軸回転四元数
+Quaternion Quaternion::RotateAxis(Vector3 vec, float angle) {
+	Quaternion out;
+	vec.Normalize();
+
+	out = RotateAxis(vec.x,vec.y,vec.z, angle);
+
+	return out;
+}
+Quaternion Quaternion::RotateAxis(float x, float y, float z, float angle) {
+
+	Quaternion out;
+	Quaternion R, P, Q;
+
+	//Pには座標を入れる(Position)
+	//不具合が起きるとしたらここ
+	P.x = this->x;
+	P.y = this->y;
+	P.z = this->z;
+	P.w = 0;
+
+	Q.x = x * sin(angle / 2);
+	Q.y = y * sin(angle / 2);
+	Q.z = z * sin(angle / 2);
+	Q.w = cos(angle / 2);
+
+	R.x = -x * sin(angle / 2);
+	R.y = -y * sin(angle / 2);
+	R.z = -z * sin(angle / 2);
+	R.w = cos(angle / 2);
+
+	out = R*P*Q;
+
+	return out;
+}
+
+
+//４→３
+Vector3 Quaternion::ToVector3() {
+	Vector3 out = Vector3::ZERO;
+
+	float ysqr = this->y * this->y;
+	float t0 = -2 * (ysqr + this->z * this->z) + 1;
+	float t1 = 2 * (this->x * this->y - this->w * this->z);
+	float t2 = -2 * (this->x * this->z + this->w * this->y);
+	float t3 = 2 * (this->y * this->z - this->w * this->x);
+	float t4 = -2 * (this->x * this->x + ysqr) + 1;
+
+	t2 = t2 > 1.0f ? 1.0f : t2;
+	t2 = t2 < -1.0f ? -1.0f : t2;
+
+	out.x = atan2(t3, t4);
+	out.y = asin(t2);
+	out.z = atan2(t1, t0);
+
+	return out;
+}
+
+
+//オイラー角でセットできるよ
+/*void Quaternion::operator =(Vector3) {
+
+}*/
 
 Quaternion Quaternion::operator *(Quaternion q) {
 	Quaternion outQ(0,0,0,0);
@@ -74,6 +144,8 @@ Quaternion Quaternion::operator *(Quaternion q) {
 	outQ.x = newVector3.x;
 	outQ.y = newVector3.y;
 	outQ.z = newVector3.z;
+
+	const_cast<Vector3&>(this->euler) = this->ToVector3();
 
 	return outQ;
 }
@@ -90,6 +162,8 @@ Quaternion Quaternion::operator +(Quaternion q) {
 	outQ.z = this->z + q.z;
 	outQ.w = this->w + q.w;
 
+	const_cast<Vector3&>(this->euler) = this->ToVector3();
+
 	return outQ;
 }
 
@@ -104,6 +178,8 @@ Quaternion Quaternion::operator -(Quaternion q) {
 	outQ.y = this->y - q.y;
 	outQ.z = this->z - q.z;
 	outQ.w = this->w - q.w;
+
+	const_cast<Vector3&>(this->euler) = this->ToVector3();
 
 	return outQ;
 }
